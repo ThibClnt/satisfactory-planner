8a4815b0786576ec8966a27434de7fb58fcc19f5
ThibClnt <112936057+ThibClnt@users.noreply.github.com>
2023-01-13 18:55:19 +0100

    Initial commit

---
+++ satisfactory-planner/app.py
@@ -0,0 +1,13 @@
+class Application:
+
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+    def manage_events(self):
+        pass
+
+    def loop(self):
+        pass
